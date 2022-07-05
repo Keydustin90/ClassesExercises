ae24587318599113f784d8697f1b0bb905f5e4dd
Dustin Key <Keydustin90@gmail.com>
2022-07-05 09:36:07 -0500

    Classes Exercise Done

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ClassesExercise
+{
+    public class Car
+    {    
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string modle, int year)
+        {
+            Make = make;
+            Modle = modle;
+            Year = year;
+
+        }
+
+        public string Make { get; set; }
+        public string Modle { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Modle = "Fusion";
+            myCar.Year = 2020;
+
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Modle = "Mustang",
+                Year = 2015
+            };
+
+            var saab = new Car("Saab", "9.4", 2001);
+
+            var carList = new List<Car>() { myCar, ford, saab };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Modle} {vehicle.Year}");
+            }
+
+        }
+    }
+}
